90208b9af6e344006f9bbcfecd48ec97d2294278
caduxl007 <eduardo2020dev@gmail.com>
2022-03-17 00:06:53 -0300

    config global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,13 +1,6 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-  :root{
-    --small: 1.5rem;
-    --medium: 3rem;
-    --large: 5rem;
-    --xl: 10rem;
-  }
-
   * {
     margin: 0;
     padding: 0;
@@ -18,10 +11,6 @@ export default createGlobalStyle`
   html {
     scroll-behavior: smooth;
     font-size: 62.5%;
-
-    /* @media (min-width: 1981px) {
-      font-size: 80%;
-    }; */
 
     @media (max-width: 768px) {
       font-size: 57.75%;
@@ -40,32 +29,8 @@ export default createGlobalStyle`
 
   body {
     scroll-behavior: smooth;
-    background-color: #fff;
-
-    height: auto;
     text-rendering: optimizeLegibility !important;
     -webkit-font-smoothing: antialiased !important;
     -moz-osx-font-smoothing: grayscale;
-  }
-
-  p {
-    font-size: var(--small);
-  }
-
-  a {
-    text-decoration: none;
-    transition: opacity 300ms ease-in-out;
-    cursor: pointer;
-  }
-
-  button {
-    border: none;
-    outline: none;
-    cursor: pointer;
-  }
-
-  body, input, button{
-    font-family: 'Inter', 'Open Sans', 'Helvetica Neue', sans-serif;
-    font-size: 1.6rem;
   }
 `;
