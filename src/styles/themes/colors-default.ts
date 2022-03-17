653819406715a4fde14155767cb92bbc82f920b5
caduxl007 <eduardo2020dev@gmail.com>
2022-03-16 23:59:41 -0300

    config colors default

--- src/styles/themes/colors-default.ts
+++ src/styles/themes/colors-default.ts
@@ -1,3 +1,9 @@
 export const colors_default = {
   white: 'hsl(0, 0%, 100%)',
+  lime_green: 'hsl(163, 72%, 41%)',
+  bright_red: 'hsl(356, 69%, 56%)',
+  facebook: 'hsl(208, 92%, 53%)',
+  twitter: 'hsl(203, 89%, 53%)',
+  instagram: 'linear gradient hsl(37, 97%, 70%) to hsl(329, 70%, 58%)',
+  youtube: 'hsl(348, 97%, 39%)',
 };
